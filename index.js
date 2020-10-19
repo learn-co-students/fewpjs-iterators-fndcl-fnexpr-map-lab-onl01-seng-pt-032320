c87658316815b77eba989f3a35bbfc5d88236e71
pledgedavis <pledgedavis14@gmail.com>
2020-10-18 23:44:06 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+//   tutorials.upperCase.split(' ').map(function(word) {
+//   return (word.charAt(0).toUpperCase() + word.slice(1));
+//  }).join(' ');
+
+  return tutorials.map( word => {
+    let words = word.split(' ')
+    let upperCased = words.map( word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let response = upperCased.join(' ')
+    return response
+  })
 }
